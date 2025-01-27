a4d6c4949d6e507459c3d085561576a73c7d98bf
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-01-27 01:50:43 -0500

    header

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -55,7 +55,7 @@ function Header() {
 
                 <section className='buttons-welcome'>
 
-                    <h1>xoxoxoxoxox</h1>
+                    <h1>Manuel</h1>
                     <h3>Professional Taxes Management</h3>
 
                     <div className='botones'>
